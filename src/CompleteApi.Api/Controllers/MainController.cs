f5cdf92163bc2746d02dc61bc2b8484ea81afd0f
Yuri Siman <yurisimannogueira@gmail.com>
2021-10-21 23:29:24 -0300

    Implementando CustomResponse na MainController

--- src/CompleteApi.Api/Controllers/MainController.cs
+++ src/CompleteApi.Api/Controllers/MainController.cs
@@ -1,18 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using CompleteApi.Business.Interfaces;
+using CompleteApi.Business.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CompleteApi.Api.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
-       // validação de notificações de erro
+        private readonly INotificador _notificador;
 
-       // validação de ModelState
+        public MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
 
-       // validação da operação de negócios
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new 
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.ObterNotificacoes().Select(n => n.Mensagem)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotificarErroModelInvalido(modelState);
+
+            return CustomResponse();
+        }
+
+        protected void NotificarErroModelInvalido(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in erros)
+            {
+                var erroMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(erroMsg);
+            }
+                
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
     }
 }
